722d688a54056ac9572291f526e5a4318792b1fb
Selin Su Varol <selinsuvarol@gmail.com>
2022-07-07 11:21:05 +0300

    completed codealong

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,32 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [newsletter, setNewsletter] = useState(false);
 
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+  
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  function handleNewsletterChange(event) {
+    // .checked, not .value!
+    setNewsletter(event.target.checked);
+  }
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <button type="submit">Submit</button>
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input
+        type="checkbox"
+        id="newsletter"
+        onChange={handleNewsletterChange}
+        checked={newsletter}
+      />
       <button type="submit">Submit</button>
     </form>
   );
